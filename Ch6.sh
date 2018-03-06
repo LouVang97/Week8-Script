d31f73dd14e94903b352735716a6c8ed15fb6a37
LouVang97 <35350104+LouVang97@users.noreply.github.com>
2018-03-06 15:15:31 -0800

    Create Ch6.sh

---
+++ Ch6.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+#Chapter 6
+
+#Check system's runlevel
+who -r
+
+#Change direcotry 
+cd /etc/systemd
+
+#View all unit's dependencies 
+systemctl show --all
+
+#Check the current systemd comfiguration
+systemctl -p UnitPath show 
+
+#Show lists os active units 
+systemctl list-untis
+
+#Get dev-mqueue.mount as a status 
+systemctl status dev-mqueue.mount
+
+#Display recent information from thw unit's journal 
+journalctl _SYSTEMD_UNIT=dev-mqueue.mount
+
+#Check on current job 
+systemctl list-jobs
+
+#Change directory
+cd /etc/systemd system
+
+#Used nano to write units
+nano Unit.target
+
+#Inside nano Unit.target
+[Unit]
+Description=file 1
+
+[Unit]
+Description=file 2
+Wants=file1.target
+
+[Install[
+WantedBy=multi-user.target
+
+#crtl x 
+to get out of nano
+
+#Activate the file2.traget
+systemctl start Unit.target
+
+#Verify that both units are active
+systemctl status Unit.target
+
+#Enable the units
+systemctl enable Unit.targetls
+
+#nano to write socket unit 
+nano SocketUnit.echo 
+
+[Unit]
+Description=echo socket 
+
+[Socket]
+ListenStream=22222
+Accept=yes
+
+#create another nano to write service 
+nano service.echo 
+
+[Unit]
+Description=echo service 
+
+[Service]
+ExecStart=-/bin/cat
+StandardInput=socket 
+
+systemctl start SocketUnit.echo 
+telnet localhost 22222
+systemctl stop socketUnit.echo 
+
+initictl list
+initictl start job
+initictl stop job
+initictl restart job
+initictl emit job 
+initictl log-priority info 
+
+telinit 4
+telinit q
+telinit s
+
+shutdown -r +5
