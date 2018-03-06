28cb836566c49de850c7c4e337f193e409d40104
LouVang97 <35350104+LouVang97@users.noreply.github.com>
2018-03-06 14:04:51 -0800

    Create Ch1&2.sh

---
+++ Ch1&2.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+#Chapter 1 and 2
+
+#print work directory
+pwd
+
+#create directory 
+mkdir MidtermScript 
+
+#change directory 
+cd MidtermScript 
+
+#create file1 & file2
+touch file1 file2 
+
+#list file 
+ls 
+
+#define the difference between file1 and file2
+diff file1 file2
+
+#copy file1 into file2
+cp file1 file2
+
+#move file1 to file2
+mv file1 file2
+
+#find the format of file1
+file file1
+
+#create nano file
+nano echo.sh 
+
+echo "Hello"
+echo "What is your name?"
+read MY_NAME
+echo "Nice to meet you!"
+return 0 
+
+#exit nano 
+
+#display echo.sh 
+cat ./echo.sh
+
+#assign permission 
+chmod u+x echo.sh 
+
+#run echo.sh 
+./echo.sh 
+
+#echo path 
+echo $PATH 
+
+#assign hello as a value 
+STUFF=hello 
+
+#send STUFF 
+export STUFF 
+
+#echo STUFF 
+echo $STUFF 
+
+
