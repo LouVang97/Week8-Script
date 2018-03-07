a8712223add5baaf8487e3ae8b5b9cfa781f865c
LouVang97 <35350104+LouVang97@users.noreply.github.com>
2018-03-06 16:01:08 -0800

    Create Ch3.sh

---
+++ Ch3.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#Chapter 3
+
+#create files 
+touch file7 file8
+
+#echo /dev/null
+echo blah blah > /dev/null
+
+#list contents of directory 
+ls -l
+
+#copies data in block of a fixed size
+dd if=file87 of=fiile88 ibs=1k obs=1k count=2
+
+#allow administrator as root 
+sudo /lib/udev/ata_id --export /dev/sda
