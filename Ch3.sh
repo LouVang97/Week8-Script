be09aa45241d6c190ec3b0c2e778e53c327a9d29
LouVang97 <35350104+LouVang97@users.noreply.github.com>
2018-03-07 21:18:58 -0600

    Update Ch3.sh

--- Ch3.sh
+++ Ch3.sh
@@ -7,9 +7,6 @@ touch file7 file8
 #send blah blah
 echo blah blah > /dev/null
 
-#list contents of directory 
-ls -l
-
 #create files and check to block device
 dd if=file7 of=fiile8 ibs=1k obs=1k count=2
 
